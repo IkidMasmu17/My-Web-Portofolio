35742bf316ec7d1d10a531694e1059986ce7bb82
= <=>
2022-11-02 12:26:28 +0700

    upload file to github

---
+++ dist/js/script.js
@@ -0,0 +1,6 @@
+// buat hamburger
+const hamburger = document.querySelector('#hamburger');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+});
